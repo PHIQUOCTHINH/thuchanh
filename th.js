6f6f2023c0e30daa20c38cf8365126b10f07355c
Thinh Phi <PHIQUOCTHINH>
2022-05-07 20:20:36 +0700

    them file

---
+++ th.js
@@ -0,0 +1,13 @@
+// function showMessage(){
+    // alert("Xin chao") }/
+
+// function showMessage(){
+//    document.getElementById("messageDiv").innerHTML= "xin chao";
+//
+// // }
+// function showMessage(){
+//     document.write("xin chao");
+// }
+function showMessage(){
+    console.log("xin chao");
+}
